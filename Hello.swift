5ba3a538f36d068402b4a24bb76b5e90abb836d0
aalshawa97 <abdullahalshawa@gmail.com>
2021-11-07 12:12:22 -0800

    Added switch case

--- Hello.swift
+++ Hello.swift
@@ -1,1 +1,16 @@
-print("Hello")
+//Abdullah Mutaz Alshawa
+//11/7/2021
+//Playground - noun: a place where people can play
+//import UIKit
+
+var someCharacter:Character = "c"
+
+switch someCharacter {
+	case "a":
+		print("is an A")
+	case "b", "c":
+		print("is a B or C")
+	default:
+		print("some fallback")
+}
+print("Hello from Swift!")
