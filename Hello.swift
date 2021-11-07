e6e41a2c52dd42d1604b3f1837485b4f6828612c
aalshawa97 <abdullahalshawa@gmail.com>
2021-11-07 13:50:54 -0800

    Subtract two numbers

--- Hello.swift
+++ Hello.swift
@@ -3,6 +3,27 @@
 //Playground - noun: a place where people can play
 //import UIKit
 
+func addTwoNumbers() {
+	let a = 2
+	let b = 2
+	let c = a + b
+
+	print(c)
+}
+
+func subtractTwoNumbers(){
+	let a = 1
+	let d = 1
+	let e = a - d
+	print(e)
+}
+
+addTwoNumbers()
+
+addTwoNumbers()
+
+subtractTwoNumbers()
+
 var someCharacter:Character = "c"
 
 var str = "Hello from Swift!"
