91e76dd4d80d726c7ca19dc08ff46d629720b41c
aalshawa97 <abdullahalshawa@gmail.com>
2021-11-07 15:22:43 -0800

    Blog post

--- Hello.swift
+++ Hello.swift
@@ -3,6 +3,30 @@
 //Playground - noun: a place where people can play
 //import UIKit
 
+class BlogPost {
+	var title = ""
+	var body = ""
+	var author = ""
+	var numberOfComments = 0
+
+	func addComment() {
+		numberOfComments += 1
+	}
+}
+
+let myPost = BlogPost()
+myPost.title = "Hello Playground"
+myPost.author = "Chris Ching"
+myPost.body = "Hello"
+myPost.addComment()
+print(myPost.numberOfComments)
+
+let mySecondPost = BlogPost()
+mySecondPost.title = "Goodbye Playground"
+mySecondPost.author = "John Travolta"
+mySecondPost.body = "Hello"
+print(mySecondPost.numberOfComments)
+
 func addTwoNumbers() {
 	let a = 2
 	let b = 2
